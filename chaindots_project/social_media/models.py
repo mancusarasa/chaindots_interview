0e2faea3360f7a38fd3ec18ae34b3e5eda837893
Pablo Salinas <salinas.pablom@gmail.com>
2024-12-03 19:22:28 -0300

    Rename some fields in the models

--- chaindots_project/social_media/models.py
+++ chaindots_project/social_media/models.py
@@ -10,13 +10,43 @@ class User(models.Model):
         return f'{self.username}'
 
 
+class Following(models.Model):
+    follower_id = models.ForeignKey(
+        User,
+        related_name='asd',
+        on_delete=models.CASCADE
+    )
+    followed_id = models.ForeignKey(
+        User,
+        related_name='qwe',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f'{self.follower_id} -> {self.followed_id}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=[
+                    'follower_id',
+                    'followed_id'
+                ],
+                name='unique_follower_followed'
+            )
+        ]
+
+
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.CharField(max_length=100, blank=False)
     creation_date = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering = ['creation_date']
+
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.CharField(max_length=100, blank=False)
