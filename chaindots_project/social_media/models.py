63684cd4620c4583cc1b742b03057ba2ee871a94
Pablo Salinas <salinas.pablom@gmail.com>
2024-12-04 01:31:18 -0300

    Remove definition of User model

--- chaindots_project/social_media/models.py
+++ chaindots_project/social_media/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 
-
-class User(models.Model):
-    username = models.CharField(max_length=100, blank=False)
-    email = models.CharField(max_length=100, blank=False)
-    password = models.CharField(max_length=100, blank=False)
-
-    def __str__(self):
-        return f'{self.username}'
+from django.contrib.auth.models import User
 
 
 class Following(models.Model):
