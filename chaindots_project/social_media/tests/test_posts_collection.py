39c35b737a278c091488a7d3628f84a5529b92eb
Pablo Salinas <salinas.pablom@gmail.com>
2024-12-05 14:32:12 -0300

    Add test case to verify filter posts by author_id

--- chaindots_project/social_media/tests/test_posts_collection.py
+++ chaindots_project/social_media/tests/test_posts_collection.py
@@ -18,6 +18,22 @@ class TestPostsCollection(APITestCase):
         user_details = self._get_user_details(new_user["id"], token)
         self.assertEqual(user_details["total_posts"], 2)
 
+    def test_fetching_posts_filtering_by_author_returns_correct_set(self):
+        user_one = self._create_user(1)
+        user_two = self._create_user(2)
+        token_one = self._login("user1", "pass1")
+        token_two = self._login("user2", "pass2")
+        posts_user_one = [self._create_post(i, token_one) for i in range(2)]
+        posts_user_two = [self._create_post(i, token_two) for i in range(3)]
+        fetched_posts_one = self._fetch_posts(user_one["id"], token_one)
+        fetched_posts_two = self._fetch_posts(user_two["id"], token_one)
+        self.assertEqual(len(fetched_posts_one["results"]), 2)
+        for post in fetched_posts_one["results"]:
+            self.assertEqual(post["author_id"], user_one["id"])
+        self.assertEqual(len(fetched_posts_two["results"]), 3)
+        for post in fetched_posts_two["results"]:
+            self.assertEqual(post["author_id"], user_two["id"])
+
     def _login(self, username: str, password: str):
         return self.client.post(
             reverse("login"),
@@ -48,11 +64,21 @@ class TestPostsCollection(APITestCase):
         )
         return response.data
 
-    def _get_user_details(self, user_id: int, token) -> Dict[str, Any]:
+    def _get_user_details(self, user_id: int, token: str) -> Dict[str, Any]:
         return self.client.get(
             reverse(
                 "user-details",
                 kwargs={'pk': user_id}
             ),
             headers={"Authorization": f"Token {token}"}
+        ).data
+
+    def _fetch_posts(self, author_id: int, token: str) -> List[Dict]:
+        return self.client.get(
+            reverse(
+                'posts-list',
+            ),
+            data={'author_id': author_id},
+            headers={"Authorization": f"Token {token}"},
+            format="json"
         ).data
