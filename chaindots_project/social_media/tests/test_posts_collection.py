72df195fa0ce676cfafa673982c6c1663c005b2d
Pablo Salinas <salinas.pablom@gmail.com>
2024-12-08 12:07:23 -0800

    Add test for a search without author_id

--- chaindots_project/social_media/tests/test_posts_collection.py
+++ chaindots_project/social_media/tests/test_posts_collection.py
@@ -1,7 +1,8 @@
 from typing import (
     List,
     Dict,
-    Any
+    Any,
+    Optional,
 )
 
 from django.urls import reverse
@@ -25,14 +26,27 @@ class TestPostsCollection(APITestCase):
         token_two = self._login("user2", "pass2")
         posts_user_one = [self._create_post(i, token_one) for i in range(2)]
         posts_user_two = [self._create_post(i, token_two) for i in range(3)]
-        fetched_posts_one = self._fetch_posts(user_one["id"], token_one)
-        fetched_posts_two = self._fetch_posts(user_two["id"], token_one)
+        fetched_posts_one = self._fetch_posts(token_one, user_one["id"])
+        fetched_posts_two = self._fetch_posts(token_one, user_two["id"])
         self.assertEqual(len(fetched_posts_one["results"]), 2)
         for post in fetched_posts_one["results"]:
             self.assertEqual(post["author_id"], user_one["id"])
         self.assertEqual(len(fetched_posts_two["results"]), 3)
         for post in fetched_posts_two["results"]:
             self.assertEqual(post["author_id"], user_two["id"])
+
+    def test_fetching_posts_without_author_retrieves_them_all(self):
+        users_count = 10
+        users = [self._create_user(i+1) for i in range(users_count)]
+        tokens = [self._login(user["username"], f"pass{i+1}") for i, user in enumerate(users)]
+        for user, token in zip(users, tokens):
+            self._create_post(f"{user['username']} says: Hi!", token)
+        posts = self._fetch_posts(tokens[0])
+        self.assertEqual(posts["count"], users_count)
+        for index, post in enumerate(posts["results"]):
+            author_id = index + 1
+            self.assertEqual(post["author_id"], author_id)
+            self.assertEqual(post["content"], f"user{author_id} says: Hi!")
 
     def _login(self, username: str, password: str):
         return self.client.post(
@@ -56,9 +70,7 @@ class TestPostsCollection(APITestCase):
     def _create_post(self, post_content: int, token: str) -> Dict[str, Any]:
         response = self.client.post(
             reverse('posts-list'),
-            {
-                "content": f"content of post {post_content}"
-            },
+            { "content": post_content },
             headers={"Authorization": f"Token {token}"},
             format="json"
         )
@@ -73,12 +85,15 @@ class TestPostsCollection(APITestCase):
             headers={"Authorization": f"Token {token}"}
         ).data
 
-    def _fetch_posts(self, author_id: int, token: str) -> List[Dict]:
+    def _fetch_posts(self, token: str, author_id: Optional[int] = None) -> List[Dict]:
+        data = {}
+        if author_id is not None:
+            data["author_id"] = author_id
         return self.client.get(
             reverse(
                 'posts-list',
             ),
-            data={'author_id': author_id},
+            data=data,
             headers={"Authorization": f"Token {token}"},
             format="json"
         ).data
